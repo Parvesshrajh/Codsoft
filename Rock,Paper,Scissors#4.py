3386145cc96625f51c15e8e11c51cbe34db8cc75
Parvesshrajh <154791786+Parvesshrajh@users.noreply.github.com>
2024-10-13 11:19:10 +0530

    Add files via upload

---
+++ Rock,Paper,Scissors#4.py
@@ -0,0 +1,29 @@
+import random
+
+options = ("rock","paper","scissors")
+playing = True
+
+while playing:
+    player = None
+    computer = random.choice(options)
+    while player not in options:
+        player = input("enter a choice(rock, paper, scissors):")
+
+    print(f"player: {player}")
+    print(f"computer: {computer}")
+
+    if player==computer:
+        print("It's a tie!")
+    elif player == "rock" and computer == "scissors":
+        print("you win")
+    elif player == "paper" and computer == "rock":
+        print("you win")
+    elif player == "scissors" and computer == "paper":
+        print("you win")
+    else:
+        print("you lose")
+
+    if not input("Play again? (y/n):").lower() == "y":
+        playing = False
+
+print("Thanks for Playing")    
